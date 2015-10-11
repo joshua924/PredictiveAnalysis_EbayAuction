58b52447cae3e00fd134f4a61242a97466f014b4
Zhaoyin Zhu <zhaoyinzhu@Zhaoyins-MBP.fios-router.home>
2015-10-11 18:23:44 -0400

    updated report

--- report/zzy/Feature_Selection.R
+++ report/zzy/Feature_Selection.R
@@ -7,20 +7,20 @@ sub.data<-subset(data,select=-c(2,3))
 
 # Entropy based filter
 weight_entropy <- information.gain(QuantitySold~., sub.data)
-print(weight_entropy)
+cbind(weight_entropy,rank(-weight_entropy))
 subset_entropy <- cutoff.k(weight_entropy, 7)
 print(subset_entropy)
 
 
 # Chi-square based filter
 weight_chi <- chi.squared(QuantitySold~., sub.data)
-print(weight_chi)
+cbind(weight_chi,rank(-weight_chi))
 subset_chi<- cutoff.k(weight_chi, 7)
 print(subset_chi)
 
 # Correlation based filter
 weight_cor<-linear.correlation(QuantitySold~., sub.data)
-print(weight_cor)
+cbind(weight_cor,rank(-weight_cor))
 subset_cor<-cutoff.k(weight_cor,7)
 print(subset_cor)
 
